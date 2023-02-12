f9bc5680922553d2247a14f6ebc146e1a29d0fae
apple <apple@apples-MacBook-Pro.local>
2023-02-12 12:10:33 +0530

    fix orientation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const arr = Array(50)
+    .fill()
+    .map((_, i) => i);
+
+  if (count > 50) {
+    setCount(0);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  const randomColor =
+    "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      {arr.map((item, idx) => (
+        <div
+          key={idx}
+          className={`child ${idx} `}
+          style={{
+            backgroundColor: `${count === item ? `${randomColor}` : ""}`,
+          }}
+        ></div>
+      ))}
     </div>
   );
 }
